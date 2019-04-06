f7cc88ddfb430e8a372d3b28dc21a561a000c006
OcaenEyes <1450136519@qq.com>
2019-04-06 18:49:20 +0800

    爬取趣图

--- spider/爬取趣图/爬取趣图.py
+++ spider/爬取趣图/爬取趣图.py
@@ -4,7 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-
+import pymysql
 
 class MZi():
 
@@ -13,7 +13,7 @@ class MZi():
 
 
     def get_pages_urls(self):
-        for i in range(4,118):
+        for i in range(1,118):
             pages_url = self.BASE_URL + 'tag/youhuo/page/'+ str(i) +'/'
             # pages_url = self.BASE_URL + 'tag/youhuo/page/'+ str(i) +'/'
             print(pages_url)
@@ -41,27 +41,76 @@ class MZi():
 
     def parse_imgaes_preview_urls(self,html):
         soup = BeautifulSoup(html,'lxml')
-        imgaes_preview_urls = soup.select("#pins li a")
         imgaes_preview_srcs = soup.select("#pins .lazy")
+        imgaes_preview = soup.select("#pins li span a")
+        imgaes_preview_dates = soup.select("#pins .time")
+
         # print(imgaes_preview_urls)
         # print(imgaes_preview_srcs)
 
-        for item in imgaes_preview_urls:
+        for item,i,k in zip(imgaes_preview,imgaes_preview_srcs,imgaes_preview_dates):
             preview_url =item.get('href')
-            print(preview_url)
-            with open('imgaes_preview_urls.txt','a') as urlf:
-                urlf.write(preview_url +','+'\n')
-        for i in imgaes_preview_srcs:
-            preview_src =i.get('data-original')
-            print(preview_src)
-            with open('imgaes_preview_srcs.txt','a') as srcf:
-                srcf.write(preview_src +','+'\n')
+            previewTitle = item.text.strip()
+            preview_src = i.get('data-original')
+            previewDate = k.text.strip()
+
+            # print(previewDate,previewTitle,preview_url,preview_src)
+
+            # #存储文件
+            # with open('imgaes_preview_urls.txt','a') as urlf:
+            #     urlf.write(preview_url +','+'\n')
+
+            self.save_images_preview(previewDate,previewTitle,preview_url,preview_src)
 
 
+    def create_table(self):
+        db = pymysql.connect('10.211.55.5','root','','test')
+        cursor = db.cursor()
+        cursor.execute("DROP TABLE IF EXISTS MeiZiTu")
+
+        sql_create_table = """CREATE TABLE MeiZiTu (
+                                    id INT NOT NULL AUTO_INCREMENT,
+                                    date CHAR (100),
+                                    title CHAR (250),
+                                    nextPage CHAR (250),
+                                    previewImg CHAR (250),
+                                    PRIMARY KEY (id))"""
+        try:
+            cursor.execute(sql_create_table)
+            db.commit()
+            print('建表成功')
+        except:
+            db.rollback()
+
+        cursor.close()
+        db.close()
+
+
+
+    def save_images_preview(self,previewDate,previewTitle,nextPage,previewSrc):
+        db = pymysql.connect('10.211.55.5', 'root', '', 'test')
+        cursor = db.cursor()
+        sql_insert_data = """INSERT INTO MeiZiTu (
+                                date,
+                                title,
+                                nextPage,
+                                previewImg) VALUES (%s,%s,%s,%s)"""
+
+        try:
+            cursor.execute(sql_insert_data,(previewDate,previewTitle,nextPage,previewSrc))
+            db.commit()
+            print('数据插入成功')
+
+        except Exception as e:
+            print("数据插入异常",e)
+            db.rollback()
+        cursor.close()
+        db.close()
 
 
 
 
 if __name__ == '__main__':
     meizi = MZi()
+    meizi.create_table()
     meizi.get_pages_urls()
