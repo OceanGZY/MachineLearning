2321914e6858ea2465baa4cdc75b1abfe0633a12
OcaenEyes <1450136519@qq.com>
2019-05-17 20:27:23 +0800

    Create 人口收入普查数据探索.py

---
+++ basicKnowledge/人口收入普查数据探索.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+'''
+    date:2019-05-05
+    version:0.1.0
+'''
+
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('./adult.data.csv')
