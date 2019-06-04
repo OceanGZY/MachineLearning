7094f4887df4034d7b81a6b4115583106390bff9
OcaenEyes <1450136519@qq.com>
2019-06-04 20:37:25 +0800

    Update 爬取二手房信息.py

--- spider/南京二手房分析/爬取二手房信息.py
+++ spider/南京二手房分析/爬取二手房信息.py
@@ -11,7 +11,7 @@ class AJK(object):
         try:
             res = requests.get(self.second_hand_house_url)
             if res.status_code == 200:
-                print(res.text)
+                self.parse_sencond_hand_house_page(res.content)
             else:
                 print('请求失败了',res.status_code)
         except Exception as e:
@@ -19,6 +19,54 @@ class AJK(object):
 
     def parse_sencond_hand_house_page(self,html):
         soup = BeautifulSoup(html,'lxml')
+        house_img_urls = soup.select('.list-item .item-img img')
+        house_titles = soup.select('.house-details .house-title a')
+        house_types = soup.select('.house-details .details-item')
+        house_prices = soup.select('.pro-price .price-det')
+        house_unit_prices = soup.select('.pro-price .unit-price')
+
+        for k,(i,j,n,l,m) in enumerate(zip(house_img_urls,house_titles,house_types,house_prices,house_unit_prices)):
+            print(i)
+            house_img_url = i.get('src')
+            print(house_img_url)
+
+            print(j)
+            house_title = j.text.strip()
+            print(house_title)
+
+            if (k % 2 == 0):
+                print(k)
+                house_type = n.text.strip().split('|')
+                if (len(house_type) > 3):
+                    style = house_type[0]
+                    area = house_type[1].strip('m²')
+                    floor = house_type[2]
+                    year = house_type[3]
+                else:
+                    print(k)
+                    style = house_type[0]
+                    area = house_type[1].strip('m²')
+                    floor = house_type[2]
+                    year = ''
+
+                print(style)
+                print(area)
+                print(floor)
+                print(year)
+                print('-----------------------------------')
+            else:
+                house_adress = n.text.strip().split('\xa0\xa0\n                    ')
+                print('地址', house_adress)
+                print('-----------------------------------')
+
+            house_price = l.text.strip('万')
+            house_price = int(house_price)
+            print(house_price)
+
+            house_unit_price = m.text.strip("元/m²")
+            house_unit_price = int(house_unit_price)
+            print(house_unit_price)
+
 
 
 
