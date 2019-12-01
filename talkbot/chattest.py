46c7e902325eaecd9b89ff99c9481175a75ea1fe
OcaenEyes <1450136519@qq.com>
2019-12-01 17:52:42 +0800

    Create chattest.py

---
+++ talkbot/chattest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+'''
+    auth:gzy
+    date:20190607
+    version:0.1.0
+'''
+
+import requests
+import itchat
+itchat.auto_login()
+itchat.send('Hello,gzy',toUserName='filehelper')
